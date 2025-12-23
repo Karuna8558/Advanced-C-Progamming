f732bb026fe5ebda896e0ed57bd42b9298492af5
Karuna8558 <684244104@mail.pbru.ac.th>
2025-12-23 14:13:21 +0700

    Implement sorting of array in main function
    Fixes include directives and sorting function.

---
+++ Lab4/lab4-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algoritim>
+
+using namespace std;
+
+int main(){
+    int A[10]= {10,2,7,4,9,5,3,1,6,8}, x;
+
+    for (const auto &x : A){
+        cout << x << " ";
+    };
+    sort(being(A), end(A), greater<int>());
+    cout << "\nAfter sorting (Ascending order):"
+    for (const auto &x : A){
+        cout << x << " ";
+    }
+}
