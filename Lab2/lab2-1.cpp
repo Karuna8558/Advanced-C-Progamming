338d85fd235f9d2ed374d22b5af5d2d8e7bd6228
Karuna8558 <684244104@mail.pbru.ac.th>
2026-01-12 14:54:09 +0700

    Create lab2-1.cpp

---
+++ Lab2/lab2-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char A[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    char B[6] = {'a', 'b', 'c', 'd', 'e', 'f'};
+    char temp;
+    string mess = "Abbef";
+    for (int i = 0; i < mess.length(); i++){
+        if (int)mess[i] >90{
+            cout << B [(int)temp - (int)'a'];
+        }else {
+            cout << A [ (int)trmp - (int)'A'];
+            }
+    }
+    return 0;
+}
